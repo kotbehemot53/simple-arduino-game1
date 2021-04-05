4ecb9186199d87a92d29fe3977bbac346aa5a8f8
Marek Woi <kot.behemotspam@gmail.com>
2021-04-05 18:05:12 +0200

    timing demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,39 @@
 #include <Arduino.h>
 
 const int FRAME_DURATION_US = 1000;
+const int FRAMES_IN_CYCLE = 2;
 
 const int LEDS_CENTER[] = {13, 8};
 
-unsigned long frame = 0;
+bool ledsCenterStates[] = {true, true};
+
+int frame = 0;
 unsigned long frameStartUs = 0;
+unsigned long elapsedMs = 0;
 
 void initFrame() {
     frameStartUs = micros();
+    elapsedMs = millis();
 }
 
 void frameUp() {
     frame++;
 
-    if (frame >= 2) {
+    if (frame >= FRAMES_IN_CYCLE) {
         frame = 0;
     }
 
     unsigned long frameEndUs = micros();
     int delayLength = FRAME_DURATION_US - (frameEndUs - frameStartUs);
     delayMicroseconds(delayLength > 0 ? delayLength : 1);
+}
+
+void multiplexLed(const int ledArray[], int ledCount, bool states[]) {
+    int currentLed = frame % ledCount;
+    digitalWrite(ledArray[currentLed], states[currentLed]);
+    for (int i = 1; i < ledCount; i++) {
+        digitalWrite(ledArray[(frame + i) % ledCount], LOW);
+    }
 }
 
 void setup() {
@@ -36,8 +49,12 @@ void setup() {
 void loop() {
     initFrame();
 
-    digitalWrite(LEDS_CENTER[frame % 2], HIGH);
-    digitalWrite(LEDS_CENTER[(frame+1) % 2], LOW);
+//    digitalWrite(LEDS_CENTER[frame % 2], HIGH);
+//    digitalWrite(LEDS_CENTER[(frame+1) % 2], LOW);
+
+    ledsCenterStates[(elapsedMs / 1000) % 2] = true;
+    ledsCenterStates[((elapsedMs / 1000) + 1) % 2] = false;
+    multiplexLed(LEDS_CENTER, 2, ledsCenterStates);
 
     frameUp();
 }
