c73d8863e4a022a12cf60d405966e3bdcb677076
Marek Woi <kot.behemotspam@gmail.com>
2021-04-08 22:33:07 +0200

    readme, comments, doc, cleanup

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
-DA SHIT
-=======
+A LITTLE GAME
+=============
+
+Rules
+-----
+
+It's a game for 2 players. There are 2 blue LEDs on top. Each player has 2 buttons - one for each LED. 
+If you press the correct button when a LED lights up, you earn a point. If you press the wrong one, 
+or do it before any LED lights up, you get 1s cooldown when you can't do anything. 
+The LEDs in the middle from green to red are point tracks for both players. When you get max points + 1 you win.
+When a point track goes blank, it means that the cooldown is in progress.
+
+Disclaimer
+----------
+
+There's no nice OOP design here and no tests. There are a lot of global variables too. I'm aware it all could
+be nicer but I did in my spare time and the program is short. I hope the comments make up for the lackings.
 
 Pin mapping
 -----------
 
 ```
-L    DIP  Ard
-blu  19   Ar13
-cp   6    Ar4
-mr   5    Ar3
-da   4    Ar2
-btnu 15   Ar9
-btnd 16   Ar10
+LEFT   DIP pin  Arduino pin
+blu    19       13
+cp     6        4
+mr     5        3
+da     4        2
+btnu   15       9
+btnd   16       10
 
-R    DIP  Ard
-blu  14   Ar8
-cp   13   Ar7
-mr   12   Ar6
-da   11   Ar5
-btnu 17   Ar11
-btnd 18   Ar12
+RIGHT  DIP pin  Arduino pin
+blu    14       8
+cp     13       7
+mr     12       6
+da     11       5
+btnu   17       11
+btnd   18       12
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,68 @@
 #include <Arduino.h>
 
-//TODO: PWM DIMMING
-//TODO: autosleep
+//TODO: PWM LED DIMMING
+//TODO: autosleep on inactivity to conserve energy
 
 /*
- * CONSTANTS & VARIABLES
+ * CONSTANTS & GLOBAL VARIABLES
  */
 
 //general constants
-const int SCORE_LED_CNT = 8;
-const int FRAME_DURATION_US = 1000;
-const int FRAMES_IN_CYCLE = SCORE_LED_CNT + 1;
-const int BUTTON_COOLDOWN_MS = 1000;
+const int SCORE_LED_CNT = 8;                   //score track length
+const int FRAME_DURATION_US = 1000;            //single frame (loop function execution) duration
+const int FRAMES_IN_CYCLE = SCORE_LED_CNT + 1; //point track shift registers cycle: 1-8 pts + 0
+const int BUTTON_COOLDOWN_MS = 1000;           //cooldown period on wrong button press
+const int SERIAL_REG_CLK_PULSE_LEN_US = 100;   //clock/reset pulse length for the shift registers
+                                               // TODO: make sure if the period can't be shorter
+const long CENTER_LED_PROBABILITY = 99900;     //probability * 100000 (so 99900 is 0,999) of one of the top LEDs lighting
+                                               // up in the current frame; current number chosen experimentally, so that
+                                               // intervals aren't so long that the game is boring but there still is some
+                                               // degree of suspense
 
 //pins
-const int LEDS_CENTER[2] = {13, 8};
-//const int LEFT_BUTTONS[2] = {9, 12};
-//const int RIGHT_BUTTONS[2] = {10, 11};
-const int BUTTONS[2][2] = {{9, 10}, {12, 11}}; //players on top, l/r leds inside
-const int SCORE_CLS[2] = {4, 7};
-const int SCORE_MRS[2] = {3, 6};
-const int SCORE_DAS[2] = {2, 5};
+const int LEDS_CENTER[2] = {13, 8};            //[ledIdx: 0 - left, 1 - right]
+const int BUTTONS[2][2] = {{9, 10}, {12, 11}}; //[playerIdx: 0 - left, 1 - right][ledIdx: 0 - left, 1 - right]
+const int SCORE_CLS[2] = {4, 7};               //[playerIdx: 0 - left, 1 - right] - clocks for shift registers
+const int SCORE_MRS[2] = {3, 6};               //[playerIdx: 0 - left, 1 - right] - /RESETs for shift registers
+const int SCORE_DAS[2] = {2, 5};               //[playerIdx: 0 - left, 1 - right] - data input for shift registers
 
 //game state
-bool ledsCenterStates[2] = {false, false};
-//bool leftButtonStates[] = {false, false};
-//bool rightButtonStates[] = {false, false};
-bool buttonStates[2][2] = {{false, false}, {false, false}}; //L/R on top, inside
-bool buttonLegality[2] = {true, true};
-bool buttonImmunity[2] = {false, false};
-unsigned long buttonIllegalityTimes[2] = {0, 0};
-int points[2] = {0, 0};
+bool ledsCenterStates[2] = {false, false};                  //[ledIdx: 0 - left, 1 - right] - whether the top leds are lit
+bool buttonStates[2][2] = {{false, false}, {false, false}}; //[playerIdx: 0 - left, 1 - right][ledIdx: 0 - left, 1 - right]
+                                                            // whether particular buttons are pressed
+bool buttonLegality[2] = {true, true};                      //[playerIdx: 0 - left, 1 - right] - if false, given user
+                                                            // can't use their buttons (happens during cooldown)
+bool buttonImmunity[2] = {false, false};                    //[playerIdx: 0 - left, 1 - right] - if true, cooldown period
+                                                            // doesn't apply to given player - instead it only lasts 1 frame;
+                                                            // it happens after receiving
+                                                            // a point, when button is still held, so that there's no penalty,
+                                                            // only a momentary release needed to be able to earn next point
+unsigned long buttonIllegalityTimes[2] = {0, 0};            //[playerIdx: 0 - left, 1 - right] - when given player's
+                                                            // buttons have become blocked (cooldown start time)
+int points[2] = {0, 0};                                     //[playerIdx: 0 - left, 1 - right] - current score for each player
 
 //rendering state
-int frame = 0;  //TODO: do we need this & FRAMES_IN_CYCLE?
+int frame = 0;
 unsigned long frameStartUs = 0;
 unsigned long frameStartMs = 0;
+
 
 
 /*
  * UTIL FUNCTIONS - VISUALS
  */
 
+/**
+ * Initialize frame (start timers to ensure constant frame length)
+ */
 void initFrame() {
     frameStartUs = micros();
     frameStartMs = millis();
 }
 
+/**
+ * Finish frame - increase frame counter (for keeping track of shift register cycle), wait to reach desired frame length
+ */
 void frameUp() {
     frame++;
 
@@ -57,7 +73,7 @@ void frameUp() {
     unsigned long frameEndUs = micros();
     int delayLength = FRAME_DURATION_US - (frameEndUs - frameStartUs);
 
-    //this is debug
+    //debug
 //    if (delayLength < 100) {
 //        Serial.print("Delay length ");
 //        Serial.println(delayLength);
@@ -66,18 +82,36 @@ void frameUp() {
     delayMicroseconds(delayLength > 0 ? delayLength : 1);
 }
 
+/**
+ * Stops the "walk" up the score track, should be called when all points for player have been displayed in given cycle
+ *
+ * @param playerIdx
+ */
 void resetScoreTrackWalkingPulse(int playerIdx) {
     digitalWrite(SCORE_MRS[playerIdx], LOW);
-    delayMicroseconds(100); //TODO: what number is right? should it pause things?
+    delayMicroseconds(SERIAL_REG_CLK_PULSE_LEN_US);
     digitalWrite(SCORE_MRS[playerIdx], HIGH);
 }
 
+/**
+ * "Walk" the score track up one step (called in every frame of the score track cycle)
+ *
+ * @param playerIdx
+ */
 void stepScoreTrackWalkingPulse(int playerIdx) {
     digitalWrite(SCORE_CLS[playerIdx], HIGH);
-    delayMicroseconds(100); //TODO: what number is right? should it pause things?
+    delayMicroseconds(SERIAL_REG_CLK_PULSE_LEN_US);
     digitalWrite(SCORE_CLS[playerIdx], LOW);
 }
 
+/**
+ * Multiplex the top LEDs
+ *
+ * @param ledArray
+ * @param ledCount
+ * @param states
+ * @param frameNumber
+ */
 void multiplexLeds(const int *ledArray, int ledCount, bool *states, int frameNumber) {
     int currentLed = frameNumber % ledCount;
     digitalWrite(ledArray[currentLed], states[currentLed]);
@@ -86,26 +120,46 @@ void multiplexLeds(const int *ledArray, int ledCount, bool *states, int frameNum
     }
 }
 
+/**
+ * Handle a single frame of a score track register
+ *
+ * @param playerIdx
+ * @param playerScore
+ * @param displayCondition
+ * @param frameNumber
+ */
 void multiplexCustomScoreViaShiftRegister(int playerIdx, byte playerScore, bool displayCondition, int frameNumber) {
+    //stop displaying points for player when all are displayed (clear the shift register)
     if (frameNumber % (SCORE_LED_CNT + 1) >= playerScore) {
         resetScoreTrackWalkingPulse(playerIdx);
     }
 
+    //send a single "1" to the shift register in the beginning of each cycle
     if ((frameNumber % (SCORE_LED_CNT + 1) == 0) && playerScore && displayCondition) {
         digitalWrite(SCORE_DAS[playerIdx], HIGH);
     } else {
         digitalWrite(SCORE_DAS[playerIdx], LOW);
     }
 
+    //1 clock pulse to the shift register
     stepScoreTrackWalkingPulse(playerIdx);
 }
 
+/**
+ * Multiplex both LED score tracks
+ */
 void multiplexScoresViaShiftRegister() {
     for (int playerIdx = 0; playerIdx < 2; playerIdx ++) {
         multiplexCustomScoreViaShiftRegister(playerIdx, points[playerIdx], buttonLegality[playerIdx], frame);
     }
 }
 
+/**
+ * Show a cool win "animation" using the LEDs to induce a reward reaction in player's brain and manipulate them
+ * into playing even more [evil laughter].
+ *
+ * @param winnerIdx
+ */
 void winAnimation(int winnerIdx) {
     //dim the loser to show who won
     for (int playerIdx = 0; playerIdx < 2; playerIdx++) {
@@ -132,28 +186,42 @@ void winAnimation(int winnerIdx) {
     }
 }
 
+/**
+ * Render regular frame stuff on all LEDs
+ */
 void renderVisuals() {
     multiplexLeds(LEDS_CENTER, 2, ledsCenterStates, frame);
     multiplexScoresViaShiftRegister();
 }
 
+
+
 /*
  * UTIL FUNCTIONS - GAME STATE
  */
 
+/**
+ * Try to initiate cooldown for a given player.
+ *
+ * @param playerIdx
+ */
 void delegalizeButton(int playerIdx) {
     buttonLegality[playerIdx] = false;
     //if there's immunity it will still be illegal in next frame if it's not released
     // (so you cant exploit the game by constantly holding it) but there'll be no added cooldown
     if (!buttonImmunity[playerIdx]) {
-//        Serial.println("no immunity!");
         buttonIllegalityTimes[playerIdx] = frameStartMs;
     } else {
-//        Serial.println("immunity!");
-        buttonIllegalityTimes[playerIdx] = 0;
+        buttonIllegalityTimes[playerIdx] = 0; //we pretend that illegality period started a long time ago so it will end immediately
     }
 }
 
+/**
+ * Check if cooldown can be ended for given player (illegality period has passed).
+ *
+ * @param playerIdx
+ * @return bool
+ */
 bool isButtonLegalizable(int playerIdx) {
     if (frameStartMs - buttonIllegalityTimes[playerIdx] > BUTTON_COOLDOWN_MS) {
         return true;
@@ -162,33 +230,43 @@ bool isButtonLegalizable(int playerIdx) {
     return false;
 }
 
+/**
+ * Try to end cooldown for a given player
+ * 
+ * @param playerIdx 
+ */
 void tryLegalizeButton(int playerIdx) {
     if (!buttonLegality[playerIdx] && isButtonLegalizable(playerIdx)) {
         buttonLegality[playerIdx] = true;
         buttonImmunity[playerIdx] = false;
-//        Serial.println("Legalized, immunity reset");
     }
 }
 
+/**
+ * Determine which of the top LEDs shall light up in this frame (if any).
+ */
 void determineLedsCenterStates() {
+    //if none of the top LEDs is lit, determine if one of them should light up in the current frame;
+    // there's a chance of it happening every frame; this could probably be improved to make gameplay more interesting.
     if (!ledsCenterStates[0] && !ledsCenterStates[1]) {
-        if (random(100000) > 99900) {
+        if (random(100000) > CENTER_LED_PROBABILITY) {
+
+            //choose which led to light up (50/50)
             byte ledToSet = random(2);
 
-            //this is debug
+            //debug
 //            Serial.print("settin led ");
 //            Serial.println(ledToSet);
 
+            //set lit state for the chosen LED
             ledsCenterStates[ledToSet] = true;
         }
     }
-
-//    this is just an example!
-//    ledsCenterStates[(frameStartMs / 1000) % 2] = true;
-//    ledsCenterStates[((frameStartMs / 1000) + 1) % 2] = false;
 }
 
-
+/**
+ * Reset the game state after a player wins
+ */
 void resetGame() {
     for (int playerIdx = 0; playerIdx < 2; playerIdx++) {
         for (int ledIdx = 0; ledIdx < 2; ledIdx++) {
@@ -203,6 +281,9 @@ void resetGame() {
     }
 }
 
+/**
+ * Read button presses
+ */
 void readButtonStates() {
     for (int playerIdx = 0; playerIdx < 2; playerIdx++) {
         for (int ledIdx = 0; ledIdx < 2; ledIdx++) {
@@ -211,34 +292,45 @@ void readButtonStates() {
     }
 }
 
+/**
+ * Handle button presses (awarod points or start cooldowns)
+ */
 void handleButtonStates() {
-    for (int playerIdx = 0; playerIdx < 2; playerIdx++) { //for each led
-        if (!buttonStates[playerIdx][0] && !buttonStates[playerIdx][1]) { //legalize player if BOTH buttons are not pressed
+    for (int playerIdx = 0; playerIdx < 2; playerIdx++) { //for each player...
+        if (!buttonStates[playerIdx][0] && !buttonStates[playerIdx][1]) { //finish cooldown if BOTH buttons are not pressed
             tryLegalizeButton(playerIdx);
         }
-        for (int ledIdx = 0; ledIdx < 2; ledIdx++) { //for each playerIdx
-            if (!ledsCenterStates[ledIdx]) {
+        for (int ledIdx = 0; ledIdx < 2; ledIdx++) { //for each of the top LEDs...
+            if (!ledsCenterStates[ledIdx]) { //if given LED is not lit and button is pressed, punis the user
                 if (buttonStates[playerIdx][ledIdx]) {
                     delegalizeButton(playerIdx);
                 }
-            } else {
+            } else { //if given LED is lit and the user isn't on cooldown, award them a point
                 if (buttonStates[playerIdx][ledIdx] && buttonLegality[playerIdx]) {
 
                     points[playerIdx]++;
-                    buttonImmunity[playerIdx] = true;
-                    ledsCenterStates[ledIdx] = false;
+                    buttonImmunity[playerIdx] = true; //turn on immunity so that the user isn't punished for holding
+                                                      // the button after the top LED goes off
+                    ledsCenterStates[ledIdx] = false; //turn the top LED off
 //                    delegalizeButton(playerIdx); //just for 1 frame - not necessary dude won't be able to release it < 1ms
 
-//                    //this is debug
+//                    //debug
+//                    Serial.print("Points: ");
 //                    Serial.println(points[playerIdx]);
 
-                    return; //don't allow the other player to get a point if he pressed the btn simultaneously (is it possible??)
+                    return; //don't allow the other player to get a point if he pressed the btn simultaneously (is it actually possible??)
                 }
             }
         }
     }
 }
 
+/**
+ * Check if win condition (max points for a player exceeded) has been reached.
+ * If yes, run a win animation to amuse the player and reset the state of the game.
+ *
+ * @return bool
+ */
 bool assertWinState() {
     for (int playerIdx = 0; playerIdx < 2; playerIdx++) {
         if (points[playerIdx] > SCORE_LED_CNT) {
@@ -251,12 +343,15 @@ bool assertWinState() {
     return false;
 }
 
+
 /**
  * THE SETUP
  */
 void setup() {
-    Serial.begin(115200);
+    //debug
+//    Serial.begin(115200);
 
+    //set all used inputs/outputs
     for (int playerIdx = 0; playerIdx < 2; playerIdx++) {
         for (int ledIdx = 0; ledIdx < 2; ledIdx++) {
             pinMode(BUTTONS[playerIdx][ledIdx], INPUT);
@@ -268,9 +363,11 @@ void setup() {
     for (int ledIdx = 0; ledIdx < 2; ledIdx++) {
         pinMode(LEDS_CENTER[ledIdx], OUTPUT);
     }
+    //initialize random seed
     randomSeed(analogRead(0));
 
-    Serial.println("oi fegit");
+    //debug
+//    Serial.println("yo yo yo");
 }
 
 /**
